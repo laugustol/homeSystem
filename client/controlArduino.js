88a320bab5e786b15c4841e5ccebdcc8b12269eb
Augusto Alvarez <augustoalvarez05@gmail.com>
2018-01-07 20:40:26 -0400

    Johny-five connected with Meteor and Arduino

--- client/controlArduino.js
+++ client/controlArduino.js
@@ -1,10 +1,21 @@
 //import {Board,Leds} from 'johnny-five';
 Template.controlArduino.onCreated(function(){
-	const five = require('johnny-five');
+	//const five = require('johnny-five');
 	//console.log(five.Board);
-	const bor = new five.Board();
+	//const bor = new five.Board();
 	/*var board = new five.Board({
 	  	repl:false
 	});
 	console.log(board);*/
+	//var five = Meteor.npmRequire('johnny-five');
+});
+Template.controlArduino.helpers({
+	isActive(e){
+		return false;
+	}
+});
+Template.controlArduino.events({
+	'click .btn'(e){
+		$(e.target).addClass("orange");
+	}
 });
